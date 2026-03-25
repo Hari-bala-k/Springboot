f7b8c606572e45dbbb02f655b6f57b155d7f592d
Hari <techiehari18hk@gmail.com>
2026-03-25 16:32:00 +0530

    new folder for springSecurity practices

---
+++ SpringSecurity/src/main/java/com/example/SpringSecurity/HelloController.java
@@ -0,0 +1,22 @@
+package com.example.SpringSecurity;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/welcome")
+    public String hello(HttpServletRequest request){
+        return "Hello " + request.getSession().getId();
+    }
+    @GetMapping("/home")
+    public String Home(HttpServletRequest request){
+        return "Home " + request.getSession().getId();
+    }
+    @GetMapping("/about")
+    public String about(){
+        return "Spring security";
+    }
+}
